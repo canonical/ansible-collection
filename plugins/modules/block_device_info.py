6cb0e792cf02a092e8a3844fcfcd21dd6ea313bd
Polona Mihalič <polona.mihalic@xlab.si>
2022-11-08 15:48:55 +0100

    Updated return documentation

--- plugins/modules/block_device_info.py
+++ plugins/modules/block_device_info.py
@@ -64,47 +64,72 @@ records:
   returned: success
   type: list
   sample:
-  - firmware_version: null # this is not in GET - checki in maas
-    system_id: y7388k
-    block_size: 102400
-    available_size: 1000000000
-    model: fakemodel
-    serial: 123
-    used_size: 0
-    tags:
-    - tag-BGt1BR
-    - tag-1Fm39m
-    - tag-Hqbbak
-    partition_table_type: null
-    partitions:
-    - uuid: 95f5d47d-0abd-408b-b3f1-c3f4df152609
-      size: 7994343424
-      bootable: false
-      tags: []
-      path: "/dev/disk/by-dname/vda-part1
-      system_id: ccrfnk
-      used_for: ext4 formatted filesystem mounted at /
-      filesystem:
-        fstype: ext4
-        label: root
-        uuid: f698b5ee-d53c-4538-86cf-ee4b23d37db6
-        mount_point: /
-        mount_options: null
-      id: 1
-      type: partition
-      device_id: 1
-      resource_uri: /MAAS/api/2.0/nodes/ccrfnk/blockdevices/1/partition/1
-    path: /dev/disk/by-dname/newblockdevice
-    size: 1000000000
-    id_path: null
-    filesystem: null
-    storage_pool: null
-    name: newblockdevice
-    used_for: Unused
-    id: 73
-    type: physical
-    uuid: null
-    resource_uri: /MAAS/api/2.0/nodes/y7388k/blockdevices/73/
+  - "available_size": 0,
+    "block_size": 512,
+    "filesystem": null,
+    "firmware_version": "2.5+",
+    "id": 146,
+    "id_path": "/dev/disk/by-id/scsi-SQEMU_QEMU_HARDDISK_lxd_root",
+    "model": "QEMU HARDDISK",
+    "name": "sda",
+    "numa_node": 0,
+    "partition_table_type": "GPT",
+    "partitions": [
+      {
+        "bootable": true,
+        "device_id": 146,
+        "filesystem": {
+            "fstype": "fat32",
+            "label": "efi",
+            "mount_options": null,
+            "mount_point": "/boot/efi",
+            "uuid": "42901672-60cb-44a3-bb8d-14f3314869c2"
+        },
+        "id": 83,
+        "path": "/dev/disk/by-dname/sda-part1",
+        "resource_uri": "/MAAS/api/2.0/nodes/grydgf/blockdevices/146/partition/83",
+        "size": 536870912,
+        "system_id": "grydgf",
+        "tags": [],
+        "type": "partition",
+        "used_for": "fat32 formatted filesystem mounted at /boot/efi",
+        "uuid": "6f3259e0-7aba-442b-9c31-5b6bafb4796a"
+      },
+      {
+        "bootable": false,
+        "device_id": 146,
+        "filesystem": {
+            "fstype": "ext4",
+            "label": "root",
+            "mount_options": null,
+            "mount_point": "/",
+            "uuid": "f74d9bc7-a2f1-4078-991c-68696794ee27"
+        },
+        "id": 84,
+        "path": "/dev/disk/by-dname/sda-part2",
+        "resource_uri": "/MAAS/api/2.0/nodes/grydgf/blockdevices/146/partition/84",
+        "size": 7457472512,
+        "system_id": "grydgf",
+        "tags": [],
+        "type": "partition",
+        "used_for": "ext4 formatted filesystem mounted at /",
+        "uuid": "eeba59a0-be8a-4be3-be15-e2858afa8487"
+      }
+    ],
+    "path": "/dev/disk/by-dname/sda",
+    "resource_uri": "/MAAS/api/2.0/nodes/grydgf/blockdevices/146/",
+    "serial": "lxd_root",
+    "size": 8000004096,
+    "storage_pool": "default",
+    "system_id": "grydgf",
+    "tags": [
+      "rotary",
+      "1rpm"
+    ],
+    "type": "physical",
+    "used_for": "GPT partitioned with 2 partitions",
+    "used_size": 7999586304,
+    "uuid": null
 """
 
 from ansible.module_utils.basic import AnsibleModule
@@ -123,9 +148,7 @@ def run(module, client: Client):
         block_device = BlockDevice.get_by_name(
             module, client, machine.id, must_exist=True
         )
-        response = [
-            block_device.get(client)
-        ]  # replace with to_ansible if all the data is stored in an object
+        response = [block_device.get(client)]
     else:
         response = client.get(f"/api/2.0/nodes/{machine.id}/blockdevices/").json
     return response
