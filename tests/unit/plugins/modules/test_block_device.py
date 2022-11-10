a74fd4b1f98cd72fa022b5d6277c56625342b154
Polona Mihalič <polona.mihalic@xlab.si>
2022-11-10 17:12:47 +0100

    Update unit tests for module

--- tests/unit/plugins/modules/test_block_device.py
+++ tests/unit/plugins/modules/test_block_device.py
@@ -14,6 +14,7 @@ from ansible_collections.canonical.maas.plugins.modules import block_device
 from ansible_collections.canonical.maas.plugins.module_utils.block_device import (
     BlockDevice,
 )
+from ansible_collections.canonical.maas.plugins.module_utils.partition import Partition
 
 pytestmark = pytest.mark.skipif(
     sys.version_info < (2, 7), reason="requires python2.7 or higher"
@@ -170,7 +171,7 @@ class TestDataForCreateBlockDevice:
         assert data == dict(
             name="my-block-device",
             id_path="/dev/vdb",
-            size=27 * 1000000,
+            size=27 * 1024 * 1024 * 1024,
             block_size=600,
             model="model",
             serial="serial",
@@ -199,26 +200,8 @@ class TestDataForCreateBlockDevice:
 
         assert data == dict(
             name="my-block-device",
-            size=27 * 1000000,
+            size=27 * 1024 * 1024 * 1024,
             block_size=512,
-        )
-
-
-class TestDataForCreatePartition:
-    def test_data_for_create_partition(self):
-        partition = dict(
-            size_gigabytes=10,
-            fs_type="ext4",
-            label="media",
-            mount_point="/media",
-            bootable=True,
-        )
-
-        data = block_device.data_for_create_partition(partition)
-
-        assert data == dict(
-            size=10 * 1000000,
-            bootable=True,
         )
 
 
@@ -246,15 +229,19 @@ class TestDataForUpdateBlockDevice:
                         label="media",
                         mount_point="/media",
                         bootable=True,
+                        mount_options="options",
+                        tags=["partition1", "bootable"],
                     ),
                     dict(
                         size_gigabytes=15,
                         fs_type="ext4",
-                        mount_point="storage",
+                        label="storage",
+                        mount_point="/storage",
                         bootable=False,
-                        tags="/dev/vdb",
+                        mount_options="mount_options",
+                        tags=["partition2", "not-bootable"],
                     ),
-                ],  # partitions need id to be updated
+                ],
                 tags=["ssd"],
                 block_size=600,
                 model="new_model",
@@ -267,7 +254,7 @@ class TestDataForUpdateBlockDevice:
             serial="old_serial",
             id_path="old_path",
             block_size=512,
-            size=12000000,
+            size=12 * 1024 * 1024 * 1024,
         )
 
         data = block_device.data_for_update_block_device(module, old_block_device)
@@ -278,5 +265,163 @@ class TestDataForUpdateBlockDevice:
             serial="new_serial",
             id_path="/dev/vdb",
             block_size=600,
-            size=27000000,
+            size=27 * 1024 * 1024 * 1024,
         )
+
+
+class TestMustUpdatePartition:
+    def test_must_update_partition_different_length(self, create_module):
+        module = create_module(
+            params=dict(
+                cluster_instance=dict(
+                    host="https://0.0.0.0",
+                    token_key="URCfn6EhdZ",
+                    token_secret="PhXz3ncACvkcK",
+                    customer_key="nzW4EBWjyDe",
+                ),
+                machine_fqdn="block-device-test.maas",
+                name="my-block-device",
+                state="present",
+                partitions=[
+                    dict(
+                        size_gigabytes=10,
+                        fs_type="ext4",
+                        label="media",
+                        mount_point="/media",
+                        bootable=True,
+                        mount_options="options",
+                        tags=["partition1", "bootable"],
+                    ),
+                    dict(
+                        size_gigabytes=15,
+                        fs_type="ext4",
+                        label="storage",
+                        mount_point="/storage",
+                        bootable=False,
+                        mount_options="mount_options",
+                        tags=["partition2", "not-bootable"],
+                    ),
+                ],
+            )
+        )
+        old_block_device = BlockDevice(
+            partitions=[
+                Partition(
+                    size=15 * 1024 * 1024 * 1024,
+                    fstype="ext4",
+                    label="storage",
+                    mount_point="/storage",
+                    bootable=False,
+                    mount_options="mount_options",
+                    tags=["partition2", "not-bootable"],
+                )
+            ],
+        )
+
+        result = block_device.must_update_partitions(module, old_block_device)
+
+        assert result is True
+
+    def test_must_update_partition(self, create_module):
+        module = create_module(
+            params=dict(
+                cluster_instance=dict(
+                    host="https://0.0.0.0",
+                    token_key="URCfn6EhdZ",
+                    token_secret="PhXz3ncACvkcK",
+                    customer_key="nzW4EBWjyDe",
+                ),
+                machine_fqdn="block-device-test.maas",
+                name="my-block-device",
+                state="present",
+                partitions=[
+                    dict(
+                        size_gigabytes=16,
+                        fs_type="ext4",
+                        label="media",
+                        mount_point="/media",
+                        bootable=True,
+                        mount_options="options",
+                        tags=["partition1", "bootable"],
+                    ),
+                ],
+            )
+        )
+        old_block_device = BlockDevice(
+            partitions=[
+                Partition(
+                    size=10 * 1024 * 1024 * 1024,
+                    fstype="ext4",
+                    label="media",
+                    mount_point="/media",
+                    bootable=True,
+                    mount_options="options",
+                    tags=["partition1", "bootable"],
+                ),
+            ],
+        )
+
+        result = block_device.must_update_partitions(module, old_block_device)
+
+        assert result is True
+
+    def test_must_update_partition_false(self, create_module):
+        module = create_module(
+            params=dict(
+                cluster_instance=dict(
+                    host="https://0.0.0.0",
+                    token_key="URCfn6EhdZ",
+                    token_secret="PhXz3ncACvkcK",
+                    customer_key="nzW4EBWjyDe",
+                ),
+                machine_fqdn="block-device-test.maas",
+                name="my-block-device",
+                state="present",
+                partitions=[
+                    dict(
+                        size_gigabytes=10,
+                        fs_type="ext4",
+                        label="media",
+                        mount_point="/media",
+                        bootable=True,
+                        mount_options="options",
+                        tags=["partition1", "bootable"],
+                    ),
+                    dict(
+                        size_gigabytes=15,
+                        fs_type="ext4",
+                        label="storage",
+                        mount_point="/storage",
+                        bootable=False,
+                        mount_options="mount_options",
+                        tags=["partition2", "not-bootable"],
+                    ),
+                ],
+            )
+        )
+        old_block_device = BlockDevice(
+            partitions=[
+                Partition(
+                    size=10 * 1024 * 1024 * 1024,
+                    fstype="ext4",
+                    label="media",
+                    mount_point="/media",
+                    bootable=True,
+                    mount_options="options",
+                    tags=["partition1", "bootable"],
+                ),
+                Partition(
+                    size=15 * 1024 * 1024 * 1024,
+                    fstype="ext4",
+                    label="storage",
+                    mount_point="/storage",
+                    bootable=False,
+                    mount_options="mount_options",
+                    tags=["partition2", "not-bootable"],
+                ),
+            ],
+        )
+
+        result = block_device.must_update_partitions(module, old_block_device)
+
+        assert result is False
